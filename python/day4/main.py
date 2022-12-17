f7bbe32b6462db46d5ce7f5345933a01d44c4ee6
techtenk <techtenk@gmail.com>
2022-12-17 10:07:49 -0600

    part1 fixed

--- python/day4/main.py
+++ python/day4/main.py
@@ -1,10 +1,14 @@
+
+def range_inclusive(start, stop):
+    return range(start, stop+1)
+
 def part1():
     f = open("input.txt")
     count = 0
     for line in f:
         ranges = line.split(',')
-        range1 = range(*map(lambda x: int(x), ranges[0].split('-')))
-        range2 = range(*map(lambda x: int(x), ranges[1].split('-')))
+        range1 = range_inclusive(*map(lambda x: int(x), ranges[0].split('-')))
+        range2 = range_inclusive(*map(lambda x: int(x), ranges[1].split('-')))
         if set(range2).issubset(set(range1)) or set(range1).issubset(set(range2)):
             count += 1
     print("Part 1: {}".format(count))
