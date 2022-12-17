87d63ef4362fdd9be05daadad26bdac701502d80
techtenk <techtenk@gmail.com>
2022-12-17 10:09:57 -0600

    part2

--- python/day4/main.py
+++ python/day4/main.py
@@ -14,3 +14,16 @@ def part1():
     print("Part 1: {}".format(count))
 
 part1()
+
+def part2():
+    f = open("input.txt")
+    count = 0
+    for line in f:
+        ranges = line.split(',')
+        range1 = range_inclusive(*map(lambda x: int(x), ranges[0].split('-')))
+        range2 = range_inclusive(*map(lambda x: int(x), ranges[1].split('-')))
+        if set(range2).intersection(set(range1)) or set(range1).intersection(set(range2)):
+            count += 1
+    print("Part 2: {}".format(count))
+
+part2()
